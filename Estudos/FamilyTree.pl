edca1e87eda599cef34e07982ef8d001b851ce65
Ítalo Santana <italoeugenio539@gmail.com>
2025-03-20 14:41:39 -0300

    teste

--- Estudos/FamilyTree.pl
+++ Estudos/FamilyTree.pl
@@ -8,6 +8,14 @@ mae(wanessa,edrick).
 mae(guia,wanessa).
 mae(dora,eric).
 mae(guia,jane).
+pai(laercio,karime).
+mae(guia, karime).
+pai(laercio,flavia).
+mae(guia, flavia).
+pai(laercio,fabia).
+mae(guia, fabia).
+pai(italo, ana).
+mae(barbara, ana).
 
 % Avos por parte de pai
 avo(X,Y) :- pai(X,Z), pai(Z,Y).
@@ -21,3 +29,8 @@ irmao(X,Y) :- pai(P,X), pai(P,Y), mae(M,X), mae(M,Y), X \= Y.
 
 % Parte de mae
 tia(X,Y) :- irmao(X,M), mae(M,Y).
+
+% irmao(wanessa, R)
+% ?- findall(R, irmao(wanessa, R), Irmaos_wanessa)
+% findall(R, irmao(wanessa, R), Irmaos_wanessa), sort(Irmaos_wanessa,S)
+% docker run -it --rm -v "diretorio raiz do arquivo pl":/app -w /app swipl swipl -s FamilyTree.pl
