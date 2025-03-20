18f89073287fe438346768d054b3a0eaecc400a9
Ítalo Santana <italoeugenio539@gmail.com>
2025-03-20 10:53:17 -0300

    studies about my family tree

---
+++ Estudos/FamilyTree.pl
@@ -0,0 +1,23 @@
+pai(rai,eric).
+pai(eric, italo).
+pai(eric, edrick).
+pai(laercio,wanessa).
+pai(laercio,jane).
+mae(wanessa,italo).
+mae(wanessa,edrick).
+mae(guia,wanessa).
+mae(dora,eric).
+mae(guia,jane).
+
+% Avos por parte de pai
+avo(X,Y) :- pai(X,Z), pai(Z,Y).
+avo(X,Y) :- mae(X,Z), pai(Z,Y).
+
+% Avos parte de mae
+avo(X,Y) :- pai(X,Z), mae(Z,Y).
+avo(X,Y) :- mae(X,Z), mae(Z,Y).
+
+irmao(X,Y) :- pai(P,X), pai(P,Y), mae(M,X), mae(M,Y), X \= Y.
+
+% Parte de mae
+tia(X,Y) :- irmao(X,M), mae(M,Y).
